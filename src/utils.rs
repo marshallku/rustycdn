f2f23cea48da4efa8c6d73232feb4dd023556dec
Marshall Ku <marshall@kakao.com>
2024-02-09 20:05:13 +0900

    Fix runtime issues with edge cases

--- src/utils.rs
+++ src/utils.rs
@@ -1,7 +1,7 @@
 pub fn get_resize_width_from_path(path: &str) -> Option<u32> {
     return path
         .split('.')
-        .find(|s| s.starts_with("w"))
+        .rfind(|s| s.starts_with("w"))
         .and_then(|s| s.strip_prefix("w"))
         .and_then(|s| s.parse::<u32>().ok());
 }
@@ -9,11 +9,11 @@ pub fn get_resize_width_from_path(path: &str) -> Option<u32> {
 pub fn get_original_path(path: &str, has_resize: bool) -> String {
     let extension = path.split('.').last().unwrap_or("");
     let original_path = if has_resize {
-        path.to_string()
-    } else {
         path.split('.').collect::<Vec<&str>>()[..path.split('.').count() - 2].join(".")
             + "."
             + extension
+    } else {
+        path.to_string()
     };
 
     return original_path;
