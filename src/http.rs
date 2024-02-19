3cf6298eec7bc6c1af918501b401c105bd1c845b
Marshall Ku <marshall@kakao.com>
2024-02-20 08:00:11 +0900

    Simplify cache header generation

--- src/http.rs
+++ src/http.rs
@@ -6,24 +6,21 @@ use axum::{
 use std::path::PathBuf;
 use tokio_util::io::ReaderStream;
 
-pub fn get_headers_without_cache() -> HeaderMap {
+pub fn get_cache_header(age: u32) -> HeaderMap {
     let mut headers = HeaderMap::new();
+    let cache_age = if age <= 0 {
+        "no-cache"
+    } else {
+        "public, max-age=31536000"
+    };
 
-    headers.insert("Cache-Control", "no-cache".parse().unwrap());
-
-    headers
-}
-
-pub fn get_headers_with_cache() -> HeaderMap {
-    let mut headers = HeaderMap::new();
-
-    headers.insert("Cache-Control", "public, max-age=31536000".parse().unwrap());
+    headers.insert("Cache-Control", cache_age.parse().unwrap());
 
     headers
 }
 
 pub fn response_error(status_code: StatusCode) -> Response {
-    (status_code, get_headers_without_cache()).into_response()
+    (status_code, get_cache_header(0)).into_response()
 }
 
 pub async fn response_file(file_path: &PathBuf) -> Response {
@@ -36,5 +33,5 @@ pub async fn response_file(file_path: &PathBuf) -> Response {
     let stream = ReaderStream::new(file);
     let body = Body::from_stream(stream);
 
-    (get_headers_with_cache(), body).into_response()
+    (get_cache_header(31536000), body).into_response()
 }
