83eddbb8ddbee67b759294f09280cd4e5127979a
Marshall Ku <marshall@kakao.com>
2024-09-04 08:52:46 +0900

    Verify existence of file

--- src/controllers/__tests__/images.rs
+++ src/controllers/__tests__/images.rs
@@ -1,6 +1,8 @@
 #[cfg(test)]
 mod tests {
 
+    use std::path::PathBuf;
+
     use axum::{
         body::Body,
         http::{Request, StatusCode},
@@ -9,7 +11,7 @@ mod tests {
     };
     use tower::ServiceExt;
 
-    use crate::{controllers::images, env::state::AppState};
+    use crate::{constants::CDN_ROOT, controllers::images, env::state::AppState};
 
     const URI: &str = "/images";
 
@@ -18,20 +20,20 @@ mod tests {
         let app: Router<AppState> =
             Router::new().route(&format!("{}/*path", URI), get(images::get));
         let state = AppState::from_env();
+        let file_path = "/images/hpp/ic_wahlberg_product_core_48.png8.png";
         let response = app
             .with_state(state)
             .oneshot(
                 Request::builder()
-                    .uri(format!(
-                        "{}/images/hpp/ic_wahlberg_product_core_48.png8.png",
-                        URI
-                    ))
+                    .uri(format!("{}{}", URI, file_path))
                     .body(Body::empty())
                     .unwrap(),
             )
             .await
             .unwrap();
+        let local_file_path = PathBuf::from(format!("{}{}{}", CDN_ROOT, URI, file_path));
 
         assert_eq!(response.status(), StatusCode::OK);
+        assert!(local_file_path.exists());
     }
 }
