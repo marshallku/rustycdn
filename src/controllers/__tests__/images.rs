ce8e4e053df38bd661fb6a92c139ccd635501d98
Marshall Ku <marshall@kakao.com>
2024-09-04 09:28:05 +0900

    Verify logics for converting files to webp image

--- src/controllers/__tests__/images.rs
+++ src/controllers/__tests__/images.rs
@@ -54,4 +54,25 @@ mod tests {
         assert_eq!(response.status(), StatusCode::NOT_FOUND);
         assert!(!local_file_path.exists());
     }
+
+    #[tokio::test]
+    async fn test_response_webp_file() {
+        let app = app();
+        let state = AppState::from_env();
+        let file_path = "/images/hpp/ic_wahlberg_product_core_48.png8.png.webp";
+        let response = app
+            .with_state(state)
+            .oneshot(
+                Request::builder()
+                    .uri(format!("{}{}", URI, file_path))
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+        let local_file_path = PathBuf::from(format!("{}{}{}", CDN_ROOT, URI, file_path));
+
+        assert_eq!(response.status(), StatusCode::OK);
+        assert!(local_file_path.exists());
+    }
 }
